1bbb8ef575d2e8fb779d92330c20f25d5afac927
yogesh1612 <yogesh1612@users.noreply.github.com>
2021-01-19 13:54:31 +0530

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -5,5 +5,6 @@ if (!require(dplyr)) {install.packages("dplyr")};  library(dplyr)
 if (!require(fastDummies)) {install.packages("fastDummies")};  library(fastDummies)
 if (!require(Hmisc)) {install.packages("Hmisc")};  library(Hmisc)
 if (!require(VIM)) {install.packages("VIM")};  library(VIM)
+if (!require(shinyWidgets)) {install.packages("shinyWidgets")};  library(shinyWidgets)
 
 if (!require(datasets)) {install.packages("datasets")};  library(datasets)
